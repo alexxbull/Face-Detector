915615a1f986195dd77d0d5571b5019ad3d5371f
Alexx Bull <alexxbull@outlook.com>
2018-08-13 13:36:29 -0700

    renamed react files to use .jsx extension

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className={styles.credits}>
+        Logo made by
+        <a
+          href="https://www.flaticon.com/authors/smashicons"
+          title="Smashicons"
+        >
+          Smashicons
+        </a>
+        from
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+        is licensed by
+        <a
+          href="http://creativecommons.org/licenses/by/3.0/"
+          title="Creative Commons BY 3.0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CC 3.0 BY
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "../styles/Header.module.css";
+
+const Header = ({ isSignedIn, changeRoute }) => {
+  const signOut = () => changeRoute("signin");
+
+  return (
+    <nav className={styles.login}>
+      {isSignedIn ? (
+        <button className={styles.loginButton} onClick={signOut}>
+          Sign out
+        </button>
+      ) : (
+        <div>
+          <button
+            className={styles.loginButton}
+            onClick={() => changeRoute("signin")}
+          >
+            Sign in
+          </button>
+          <button
+            className={styles.loginButton}
+            onClick={() => changeRoute("register")}
+          >
+            Register
+          </button>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
